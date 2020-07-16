5e05652bcd8e183b2383ed7f9756c22158c6eec1
hasparus <hasparus@gmail.com>
2020-07-16 13:08:18 +0200

    Add Theme UI, TypeScript and Theme UI Prism

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,7 @@
-/**
- * Configure your Gatsby site with this file.
- *
- * See: https://www.gatsbyjs.org/docs/gatsby-config/
- */
-
 module.exports = {
-  /* Your site config here */
-  plugins: [],
+  plugins: [
+    "gatsby-plugin-theme-ui",
+    "gatsby-plugin-typescript",
+    "gatsby-plugin-mdx",
+  ],
 }
